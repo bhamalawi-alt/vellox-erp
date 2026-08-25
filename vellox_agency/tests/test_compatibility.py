45fcae0248ab950b6525463568bff4be037bdd7e
imac <imac@Raed.local>
2026-08-25 04:20:35 +0300

    Revert "test: temporary failing probe to verify CI enforcement"
    This reverts commit b9e666d091086153a7791301fa79f94e83da9c4b.

--- vellox_agency/tests/test_compatibility.py
+++ vellox_agency/tests/test_compatibility.py
@@ -31,8 +31,3 @@ class TestRuntimeCompatibility(FrappeTestCase):
 
 def test_deliberate_ci_failure_probe():
 	assert False, "deliberate failure: proving CI catches broken tests"
-
-
-class TestDeliberateProbe(FrappeTestCase):
-	def test_ci_enforcement_probe(self):
-		self.fail("deliberate failure: proving CI catches broken tests")
